4798381d48cb5564a26d490e317225248e6be0c9
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-07-17 10:05:30 +0200

    wip

--- functions.php
+++ functions.php
@@ -48,7 +48,7 @@ add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
 function load_custom_wp_admin_style(){
      $version = file_exists(get_template_directory() . '/version.txt') ? file_get_contents(get_template_directory() . '/version.txt') : '1.0';
-    // wp_enqueue_style( 'gutenberg',  'https://hostdashboard.nl/devdocs/css/gutenberg.css');
+    wp_enqueue_style( 'gutenberg',  'https://hostdashboard.nl/devdocs/css/gutenberg.css');
      wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), $version, 'all');
     // wp_enqueue_style( 'swiper',  'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
     // wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), '1.2', 'all');
